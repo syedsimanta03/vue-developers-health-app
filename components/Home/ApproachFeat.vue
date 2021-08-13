8043bc8d41ce7e8af65c26e655326524240a0073
sam <syed.simanta06@gmail.com>
2021-08-13 15:44:34 +0600

    first commit

---
+++ components/Home/ApproachFeat.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex flex-col text-left">
+    <img :src="img" alt='icon' />
+    <h3 class="font-bold mt-2">{{ title }}</h3>
+    <p>{{ brief }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    img: {
+      type: String,
+      required: true,
+      default: 'approach3',
+    },
+    title: {
+      type: String,
+      required: true,
+      default: 'Title',
+    },
+    brief: {
+      type: String,
+      required: true,
+      default: 'Lorem ipsum brief',
+    },
+  },
+  watch: {
+    img(val) {
+      // eslint-disable-next-line vue/no-mutating-props
+      this.img = val
+    },
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ components/Home/DiscoverItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <img :src="img" alt="icon" width="60" />
+    <h3 class="font-bold mt-6 text-lg">{{ title }}</h3>
+    <p class="mt-2 text-center">
+      {{ brief }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    img: {
+      type: String,
+      required: true,
+      default: 'feature-h-icon.svg',
+    },
+    title: {
+      type: String,
+      required: true,
+      default: 'Title',
+    },
+    brief: {
+      type: String,
+      required: true,
+      default: 'Lorem ipsum brief',
+    },
+  },
+  watch: {
+    img(val) {
+      // eslint-disable-next-line vue/no-mutating-props
+      this.img = val
+    },
+  },
+})
+</script>
+
+<style scoped></style>
