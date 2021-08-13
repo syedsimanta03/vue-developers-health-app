053f7aeed94b7f896c4a9a92f545a2704c355e6b
sam <syed.simanta06@gmail.com>
2021-08-13 16:49:36 +0600

    'added'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'serverless',
-
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Devdna',
